036411c82cc797395ad369d6fbe54bbc6e761af8
mahtabhussainofficial <mahtabhussainofficial@gmail.com>
2026-09-06 03:06:55 +0500

    Add band name generator script and update README

---
+++ band-name generator.py
@@ -0,0 +1,4 @@
+print("welcome to the Band Name Generator.")
+name_of_city = input("what is the name of the city you grew up in?\n")
+pet_name = input("what is the name of your pet?\n")
+print(f"Your band name could be {name_of_city} {pet_name}.")
--- mahtab.readme.md
+++ mahtab.readme.md
@@ -1,1 +1,2 @@
 1. i used (pyttsx3) this library
+2. i have craeted band name generator 
