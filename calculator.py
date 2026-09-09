2f342872e3c61d22076633f41365941aed2af45e
mehtabhussainofficial <mahtabhussainofficial@gmail.com>
2026-09-09 21:33:08 +0500

    add  function and update readme

---
+++ calculator.py
@@ -0,0 +1,25 @@
+print("===== Python Calculator =====")
+
+num1 = float(input("Enter first number: "))
+operator = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+if operator == "+":
+    result = num1 + num2
+
+elif operator == "-":
+    result = num1 - num2
+
+elif operator == "*":
+    result = num1 * num2
+
+elif operator == "/":
+    if num2 == 0:
+        result = "Cannot divide by zero"
+    else:
+        result = num1 / num2
+
+else:
+    result = "Invalid operator"
+
+print("Result:", result)
---
+++ function.py
@@ -0,0 +1,9 @@
+def greet(name):
+    print("hello,",name,"kaya hal ha ap ka ummed karta ho tum khairyat se ho gae")
+def num(age):
+    print("that's a nice age,",age,"is a great time for you to start python journey")
+
+name = input("what is your name : ")
+age = input("what is your age :")
+greet(name)
+num(age)
